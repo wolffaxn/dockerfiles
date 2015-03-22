6404f40e6ab29ca73143b58ea13de19d6fbaa8ba
Alexander Wolff <wolffaxn@gmail.com>
2015-03-22 19:15:40 +0100

    update jenkins version; change to java 8

--- jenkins/bin/setenv.sh
+++ jenkins/bin/setenv.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-export JAVA_HOME=/usr/java/java7
+export JAVA_HOME=/usr/java/java8
 
 export JENKINS_HOME=/var/jenkins
 
 export CATALINA_BASE=/usr/share/jenkins
 export CATALINA_HOME=/usr/share/tomcat7
-export CATALINA_OPTS="-server -Xmx1024m -XX:MaxPermSize=256m -XX:+HeapDumpOnOutOfMemoryError -Djava.awt.headless=true"
+export CATALINA_OPTS="-server -Xmx1024m -XX:MaxMetaspaceSize=256m -XX:+HeapDumpOnOutOfMemoryError -Djava.awt.headless=true"
 export CATALINA_PID="$CATALINA_BASE/tomcat.pid"
 
 export PATH=$PATH:$JAVA_HOME/bin;
