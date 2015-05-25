eda8e9aaea9432e6287047d035149b38629552f5
Alexander Wolff <wolffaxn@gmail.com>
2015-05-25 10:51:12 +0200

    add postgresql dockerfile

--- build-all.sh
+++ build-all.sh
@@ -2,6 +2,7 @@
 
 docker build --rm -t wolffaxn/base base
 docker build --rm -t wolffaxn/oracle-java8 oracle-java8
+docker build --rm -r wolffaxn/postgresql
 docker build --rm -t wolffaxn/tomcat7 tomcat7
 docker build --rm -t wolffaxn/tomcat8 tomcat8
 docker build --rm -t wolffaxn/jenkins jenkins
