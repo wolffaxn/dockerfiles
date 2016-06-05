5b02776eee8e575e025f1b2e1c59d3f3f007953d
Alexander Wolff <wolffaxn@gmail.com>
2016-06-05 09:12:34 +0200

    rework directory structure

---
+++ jenkins/centos7/bin/shutdown.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+. /usr/share/jenkins/bin/setenv.sh
+
+. $CATALINA_HOME/bin/catalina.sh stop
---
+++ nexus/centos7/bin/shutdown.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+. /usr/share/nexus/bin/setenv.sh
+
+. $CATALINA_HOME/bin/catalina.sh stop
