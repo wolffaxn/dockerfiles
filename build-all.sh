1cfd8b51f2784351ae2bcafaa8462a44fffa821c
Alexander Wolff <wolffaxn@gmail.com>
2016-06-05 21:54:22 +0200

    bugfix

--- build-all.sh
+++ build-all.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 docker build --rm -t wolffaxn/base/centos7 base
-docker build --rm -t wolffaxn/oracle-java8 oracle-java8
-docker build --rm -r wolffaxn/postgresql/centos7
+docker build --rm -t wolffaxn/java/centos7 java
+docker build --rm -r wolffaxn/postgresql/centos7 postgresql
 docker build --rm -t wolffaxn/tomcat7/centos7 tomcat7
 docker build --rm -t wolffaxn/tomcat8/centos7 tomcat8
 docker build --rm -t wolffaxn/jenkins/centos7 jenkins
