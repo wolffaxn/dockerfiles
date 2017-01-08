e125d766c33283da6331ba36cb6f4a89de2cdca1
Alexander Wolff <wolffaxn@gmail.com>
2017-01-08 21:04:58 +0100

    restructure files

--- build-all.sh
+++ build-all.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-docker build --rm -t wolffaxn/base base/centos7
-docker build --rm -t wolffaxn/java java/centos7
-docker build --rm -r wolffaxn/postgresql postgresql/centos7
-docker build --rm -t wolffaxn/jenkins jenkins/centos7
-docker build --rm -t wolffaxn/nexus nexus/centos7
-docker build --rm -t wolffaxn/sonarqube sonarqube/centos7
-docker build --rm -t wolffaxn/wildfly wildfly/centos7
+docker build --rm -t wolffaxn/base base
+docker build --rm -t wolffaxn/java java
+docker build --rm -r wolffaxn/postgresql postgresql
+docker build --rm -t wolffaxn/jenkins jenkins
+docker build --rm -t wolffaxn/nexus nexus
+docker build --rm -t wolffaxn/sonarqube sonarqube
+docker build --rm -t wolffaxn/wildfly wildfly
